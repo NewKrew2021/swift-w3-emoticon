224518b3dbf981d2d5a04c93c2b790b9026ba241
eden.joon <eden.joon@kakaocorp.com>
2021-01-18 18:47:22 +0900

    Table view 구현
    table view를 구현하고 안의 custom cell class를 이용해
    원하는 모양의 cell을 만들었다.

---
+++ EmoticonApp/EmoticonApp/EmoticonCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmoticonCell.swift
+//  EmoticonApp
+//
+//  Created by 윤준수 on 2021/01/18.
+//
+
+import UIKit
+
+class EmoticonCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var author: UILabel!
+}
