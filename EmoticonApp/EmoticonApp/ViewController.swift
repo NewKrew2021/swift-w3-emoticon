cb65c7b353cd4b3ba52243a18b9aa1aceefe927a
eden.joon <eden.joon@kakaocorp.com>
2021-01-18 17:43:15 +0900

    UIView에 라벨, 이미지 추가
    라벨, 이미지를 추가하고 라벨 줄 변경을 위해
    linebreakmode 를 hangulWordPriority로 설정함

--- EmoticonApp/EmoticonApp/ViewController.swift
+++ EmoticonApp/EmoticonApp/ViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var upperView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let patternImgage = UIImage(named: "pattern"){
+            upperView.backgroundColor = UIColor(patternImage: patternImgage)
+        }
+        
+        eventTitle.lineBreakStrategy = .hangulWordPriority
     }
 }
 
