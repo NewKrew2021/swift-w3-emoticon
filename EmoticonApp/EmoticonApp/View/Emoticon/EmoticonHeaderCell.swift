d2e4d292d8858065c7813398a8af10c7d49e895f
Tree.JI <tree.ji@kakaocorp.com>
2021-01-18 17:04:22 +0900

    Add EmoticonHeaderCell.swift and .xib

---
+++ EmoticonApp/EmoticonApp/View/Emoticon/EmoticonHeaderCell.swift
@@ -0,0 +1,12 @@
+//
+//  EmoticonHeaderCell.swift
+//  EmoticonApp
+//
+//  Created by 지현우 on 2021/01/18.
+//
+
+import UIKit
+
+class EmoticonHeaderCell: UITableViewHeaderFooterView{
+    
+}
--- EmoticonApp/EmoticonApp/View/Emoticon/EmoticonViewController.swift
+++ EmoticonApp/EmoticonApp/View/Emoticon/EmoticonViewController.swift
@@ -8,29 +8,23 @@
 import UIKit
 
 class EmoticonViewController: UIViewController {
-    
-    @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet var tableView: UITableView!
+    let nibName_Emoticon = UINib(nibName: "EmoticonTableViewCell", bundle: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
+        tableView.register(nibName_Emoticon, forCellReuseIdentifier: "emoticonTVCell")
     }
-
-
 }
 
-
-extension EmoticonViewController: UITableViewDelegate, UITableViewDataSource{
+extension EmoticonViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
-    
 }
-
