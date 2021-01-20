e40fd218f95c601ef7aee8f9eb0bb3d39e3ca568
eden.joon <eden.joon@kakaocorp.com>
2021-01-20 11:36:31 +0900

    Main View Controller의 UIView 코드를 별개의 파일로 옮겨 코드 간소화

--- EmoticonApp/EmoticonApp/Controller/MainViewController.swift
+++ EmoticonApp/EmoticonApp/Controller/MainViewController.swift
@@ -9,72 +9,30 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    private let eventMainText: UILabel = UILabel()
-    private let eventTitle: UILabel = UILabel()
-    private var mainImage: UIView = UIView()
-    private let subImage: UIImageView = UIImageView()
-    
     @IBOutlet weak var emoticonList: UITableView!
     
     private let emoticonService: EmoticonService = EmoticonService.instance
+    private let upperView = MainImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
     }
     func initView() {
-        initMainImage()
+        initUpperView()
         initTableView()
     }
     
-    func initMainImage() {
-        view.addSubview(mainImage)
-        mainImage.translatesAutoresizingMaskIntoConstraints = false
-        mainImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
-        mainImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        mainImage.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
-        mainImage.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+    func initUpperView() {
+        view.addSubview(upperView)
+        upperView.translatesAutoresizingMaskIntoConstraints = false
+        upperView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
+        upperView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        upperView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
+        upperView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
         guard let patternImgage = UIImage(named: "pattern") else { return }
-        mainImage.backgroundColor = UIColor(patternImage: patternImgage)
-        
-        initTitle()
-        initMainText()
-        initSubImage()
-    }
-    
-    func initTitle() {
-        mainImage.addSubview(eventTitle)
-        eventTitle.translatesAutoresizingMaskIntoConstraints = false
-        eventTitle.leadingAnchor.constraint(equalTo: mainImage.leadingAnchor, constant: 3).isActive = true
-        eventTitle.topAnchor.constraint(equalTo: mainImage.topAnchor, constant: 3).isActive = true
-        eventTitle.text = "이벤트"
-        eventTitle.textColor = .systemBlue
-        eventTitle.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        eventTitle.sizeToFit()
-    }
-    
-    func initMainText() {
-        mainImage.addSubview(eventMainText)
-        eventMainText.translatesAutoresizingMaskIntoConstraints = false
-        eventMainText.leadingAnchor.constraint(equalTo: eventTitle.leadingAnchor).isActive = true
-        eventMainText.centerYAnchor.constraint(equalTo: mainImage.centerYAnchor).isActive = true
-        eventMainText.widthAnchor.constraint(equalTo: mainImage.widthAnchor, multiplier: 0.4).isActive = true
-        eventMainText.text = "친구 추가하면 겨울맞이 이모티콘 선물!"
-        eventMainText.font = UIFont.systemFont(ofSize: 24)
-        eventMainText.numberOfLines = 0
-        eventMainText.sizeToFit()
-        eventMainText.lineBreakStrategy = .hangulWordPriority
-    }
-    
-    func initSubImage() {
-        mainImage.addSubview(subImage)
-        subImage.translatesAutoresizingMaskIntoConstraints = false
-        subImage.trailingAnchor.constraint(equalTo: mainImage.trailingAnchor).isActive = true
-        subImage.bottomAnchor.constraint(equalTo: mainImage.bottomAnchor).isActive = true
-        subImage.widthAnchor.constraint(equalTo: mainImage.widthAnchor, multiplier: 0.5).isActive = true
-        subImage.heightAnchor.constraint(equalTo: mainImage.heightAnchor, multiplier: 0.9).isActive = true
-        subImage.image = UIImage(named: "basket")
-        subImage.contentMode = .scaleAspectFit
+        upperView.backgroundColor = UIColor(patternImage: patternImgage)
+        upperView.initself()
     }
     
     func initTableView() {
@@ -82,7 +40,7 @@ class MainViewController: UIViewController {
         emoticonList.rowHeight = 80
         emoticonList.translatesAutoresizingMaskIntoConstraints = false
         emoticonList.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        emoticonList.topAnchor.constraint(equalTo: mainImage.bottomAnchor, constant: 10).isActive = true
+        emoticonList.topAnchor.constraint(equalTo: upperView.bottomAnchor, constant: 10).isActive = true
         emoticonList.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         emoticonList.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
     }
---
+++ EmoticonApp/EmoticonApp/View/MainImageView.swift
@@ -0,0 +1,66 @@
+//
+//  selfView.swift
+//  EmoticonApp
+//
+//  Created by 윤준수 on 2021/01/20.
+//
+
+import UIKit
+
+class MainImageView: UIView {
+    private let eventMainText: UILabel = UILabel()
+    private let eventTitle: UILabel = UILabel()
+    private let subImage: UIImageView = UIImageView()
+
+    
+    func initself() {
+//        view.addSubview(self)
+//        self.translatesAutoresizingMaskIntoConstraints = false
+//        self.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
+//        self.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        self.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30).isActive = true
+//        self.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
+//        guard let patternImgage = UIImage(named: "pattern") else { return }
+//        self.backgroundColor = UIColor(patternImage: patternImgage)
+//
+        initTitle()
+        initMainText()
+        initSubImage()
+    }
+    
+    func initTitle() {
+        self.addSubview(eventTitle)
+        eventTitle.translatesAutoresizingMaskIntoConstraints = false
+        eventTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 3).isActive = true
+        eventTitle.topAnchor.constraint(equalTo: self.topAnchor, constant: 3).isActive = true
+        eventTitle.text = "이벤트"
+        eventTitle.textColor = .systemBlue
+        eventTitle.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        eventTitle.sizeToFit()
+    }
+    
+    func initMainText() {
+        self.addSubview(eventMainText)
+        eventMainText.translatesAutoresizingMaskIntoConstraints = false
+        eventMainText.leadingAnchor.constraint(equalTo: eventTitle.leadingAnchor).isActive = true
+        eventMainText.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        eventMainText.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.4).isActive = true
+        eventMainText.text = "친구 추가하면 겨울맞이 이모티콘 선물!"
+        eventMainText.font = UIFont.systemFont(ofSize: 24)
+        eventMainText.numberOfLines = 0
+        eventMainText.sizeToFit()
+        eventMainText.lineBreakStrategy = .hangulWordPriority
+    }
+    
+    func initSubImage() {
+        self.addSubview(subImage)
+        subImage.translatesAutoresizingMaskIntoConstraints = false
+        subImage.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        subImage.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        subImage.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5).isActive = true
+        subImage.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.9).isActive = true
+        subImage.image = UIImage(named: "basket")
+        subImage.contentMode = .scaleAspectFit
+    }
+
+}
