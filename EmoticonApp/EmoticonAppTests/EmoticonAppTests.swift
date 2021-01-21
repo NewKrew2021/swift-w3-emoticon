48d1a46a915ff2738c833277186a5b448b450ae4
eden.joon <eden.joon@kakaocorp.com>
2021-01-21 13:31:10 +0900

    Make test function of cartService
    cartService의 기능을 테스트 하는 함수를 만들고 테스트 함

--- EmoticonApp/EmoticonAppTests/EmoticonAppTests.swift
+++ EmoticonApp/EmoticonAppTests/EmoticonAppTests.swift
@@ -11,10 +11,30 @@ import XCTest
 class EmoticonAppTests: XCTestCase {
     
     let cartService: CartService = CartServiceImpl.instance
-
+    
     override func setUpWithError() throws {
+        cartService.removeAll()
+        for i in 0..<10 {
+            cartService.addProduct(product: Product(emoticonName: String(i)))
+        }
     }
 
-    func testExample() throws {
+    func testCount() throws {
+        XCTAssertEqual(10, cartService.count)
+    }
+    
+    func testCartAppend() throws {
+        cartService.addProduct(product: Product(emoticonName: "11"))
+        XCTAssertEqual(11, cartService.count)
+    }
+    
+    func testCartRemoveAt() throws {
+        cartService.removeProduct(at: 0)
+        XCTAssertEqual(cartService.count, 9)
+    }
+    
+    func testCartRemoveAll() throws {
+        cartService.removeAll()
+        XCTAssertEqual(0, cartService.count)
     }
 }
